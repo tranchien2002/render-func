cdc0d777be65ffd14d00fd92e9254a98da2423b4
tranchien <chientm@apples-MacBook-Pro.local>
2020-07-15 18:21:46 +0700

    render func

--- src/index.js
+++ src/index.js
@@ -1,61 +1,57 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Fastclick from 'fastclick'
-import App from './App'
-import createStore from './store'
-import initialState from './initialState'
-import * as record from './record'
-import './css/index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Fastclick from 'fastclick';
+import App from './App';
+import createStore from './store';
+import initialState from './initialState';
+import * as record from './record';
+import './css/index.css';
 
 let state = {
-	initialState: {...initialState},
-	...initialState,
-}
+  initialState: { ...initialState },
+  ...initialState,
+};
 
-let store = createStore(state)
+let store = createStore(state);
 
 function renderToDOM(state) {
-	return ReactDOM.render(
-	  <App state={state || store.getState()} actions={store.actions} record={record} />,
-	  document.getElementById('root')
-	)
+  console.log('state', state);
+  console.log('store', store.getState());
+  return ReactDOM.render(
+    <App state={state || store.getState()} actions={store.actions} record={record} />,
+    document.getElementById('root')
+  );
 }
 
-record.setRender(renderToDOM)
+store.subscribe((data) => {
+  let { actionType, currentState } = data;
 
-store.subscribe(data => {
-	let { actionType, currentState } = data
+  if (actionType === 'START_PLAY') {
+    playing();
+    return;
+  }
 
-	if (actionType === 'START_PLAY') {
-		record.start()
-		record.save(currentState.initialState)
-		playing()
-		return
-	}
+  if (actionType === 'PLAYING' || actionType === 'FLY_UP') {
+    renderToDOM();
+    if (currentState.game.status === 'over') {
+      stopPlaying();
+    }
+    return;
+  }
+});
 
-	if (actionType === 'PLAYING' || actionType === 'FLY_UP') {
-		record.save(currentState)
-		renderToDOM()
-		if (currentState.game.status === 'over') {
-			record.finish()
-			stopPlaying()
-		}
-		return
-	}
-})
-
-let { PLAYING } = store.actions
-let requestID = null
+let { PLAYING } = store.actions;
+let requestID = null;
 function playing() {
-	requestID = requestAnimationFrame(playing)
-	PLAYING()
+  requestID = requestAnimationFrame(playing);
+  PLAYING();
 }
 function stopPlaying() {
-	cancelAnimationFrame(requestID)
+  cancelAnimationFrame(requestID);
 }
 
-renderToDOM()
+renderToDOM();
 
 if ('ontouchstart' in document) {
-	Fastclick.attach(document.body)
+  Fastclick.attach(document.body);
 }
